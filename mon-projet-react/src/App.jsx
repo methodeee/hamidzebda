1376b5b3c672e2e8296fc651b92424d52798040e
methodeee <methodeee98@gmail.com>
2025-06-21 13:49:38 +0100

    :sparkles: added football api to my front

--- mon-projet-react/src/App.jsx
+++ mon-projet-react/src/App.jsx
@@ -1,13 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Classement from "./pages/classement"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    "hello!" 
+    <div className="container">
+      <h1>FootLive</h1>
+      <Classement />
+    </div>
   )
 }
 
