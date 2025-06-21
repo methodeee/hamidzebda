5e1c124e7092ea71cffd41c4a044801c771ae4a9
methodeee <methodeee98@gmail.com>
2025-06-21 13:56:54 +0100

    Revert ":sparkles: added football api to my front"

--- mon-projet-react/src/App.jsx
+++ mon-projet-react/src/App.jsx
@@ -1,11 +1,13 @@
-import Classement from "./pages/classement"
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div className="container">
-      <h1>FootLive</h1>
-      <Classement />
-    </div>
+    "hello!" 
   )
 }
 
